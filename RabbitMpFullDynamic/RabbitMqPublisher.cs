889389cb92157d4489a713d7e2bbff19eabd56d7
taoworld <tfu_ch@yahoo.fr>
2016-04-29 09:38:13 +0200

    rabbitmq dynamically create
    exchange
    queue
    shovel

---
+++ RabbitMpFullDynamic/RabbitMqPublisher.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace RabbitMpFullDynamic
+{
+    public class RabbitMqPublisher
+    {
+        public const string Hostname = "localhost";
+
+        private const string Login = "tao";
+        private const string Password = "tao";
+        private const int Port = 5673;
+        private const string PublisherVhost = "publisherVhost";
+        private const string ConsumerVhost = "consumerVhost";
+        private static object LockInstance = new object();
+
+        private static ConnectionFactory PubliherFactoryInstance, ConsumerFactoryInstance;
+        
+
+        public static ConnectionFactory GetFactoryInstance(bool IsForPublisher)
+        {
+            var targetInstance = IsForPublisher ? PubliherFactoryInstance : ConsumerFactoryInstance;
+
+            if (targetInstance == null)
+            {
+                lock (LockInstance)
+                {
+                    if (targetInstance == null)
+                    {
+                        targetInstance = new ConnectionFactory()
+                        {
+                            HostName = Hostname,
+                            UserName = Login,
+                            Password = Password,
+                            Port = Port,
+                            VirtualHost = IsForPublisher ? PublisherVhost : ConsumerVhost
+                        };
+                    }
+                }
+            }
+
+            return targetInstance;
+        }
+
+
+        public 
+    }
+}
